14e205555030ecc73131fc60c3be0de2ef290743
WojciechTs <116261837+WojciechTs@users.noreply.github.com>
2025-01-09 17:31:17 +0100

    Create zad2.fs

---
+++ lab7/zad2.fs
@@ -0,0 +1,50 @@
+type BankAccount (AccountNumber: int, Balance :float ) =
+    let mutable _account = AccountNumber
+    let mutable _balance: float = Balance
+    member this.AccountNumber  = _account
+    member this.Balance = _balance
+    member this.Deposit (input: float) =
+        _balance <- _balance + input
+        printfn "Depozynowano %f na Account number: %i" input this.AccountNumber
+    member this.Withdraw (input) =
+        if input <= _balance then
+            _balance <- _balance - input
+            printfn "Wypłacono %f z Account number: %i" input this.AccountNumber
+        else
+            printfn "Nie ma tylu środów na Account number: %i" this.AccountNumber
+
+type Bank () =
+    let mutable _accounts: BankAccount List = []
+    member this.CreateAccount(accountNumber: int, Balance :float) =
+        let account = BankAccount(accountNumber,Balance)
+        _accounts <- account::_accounts
+    member this.GetAccount (accountNumber: int) =
+        let account = _accounts |> List.find(fun c -> c.AccountNumber = accountNumber)
+        if account.AccountNumber=accountNumber then
+            printfn "Account number: %i; Balance: %f" account.AccountNumber account.Balance
+        else    
+            printfn "Account number: %i nie znalezione!" accountNumber
+    member this.UpdateAccount (accountNumber: int, balance: float) =
+        let account = _accounts |> List.find(fun c -> c.AccountNumber = accountNumber)
+        if account.AccountNumber=accountNumber then
+            if balance <= 0 then
+                account.Deposit balance
+            else
+                account.Withdraw balance
+            printfn "Account number: %i; Balance: %f" account.AccountNumber account.Balance
+        else    
+            printfn "Account number: %i nie znaleziono!" accountNumber
+    member this.DeleteAccount (accountNumber: int) =
+        let account = _accounts |> List.findIndex(fun c -> c.AccountNumber = accountNumber)
+        if account <> -1 then
+            _accounts <- _accounts |> List.removeAt account
+            printfn "Account number: %i zostało usuniete" accountNumber
+        else
+            printfn "Account number: %i nie znaleziono" accountNumber
+         
+
+[<EntryPoint>]
+let main argv =
+
+    0
+
